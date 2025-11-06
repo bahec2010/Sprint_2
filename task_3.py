e8aa1106d8dc8eb6bf113778b99e6387a7db58d4
isovsyannikov <bahec2010@gmail.com>
2025-11-06 16:27:34 +0300

    added local variable 'points'

--- task_3.py
+++ task_3.py
@@ -1,30 +1,29 @@
 class PointsForPlace:
     @staticmethod
     def get_points_for_place(place):
+        points = 0
         if place > 100:
             print('Баллы начисляются только первым 100 участникам')
-            return None
         elif place < 1:
             print('Спортсмен не может занять нулевое или отрицательное место')
-            return None
         else:
-            return 101 -place
+            points =  101 - place
+        return points
 
 class PointsForMeters:
     @staticmethod
     def get_points_for_meters(meters):
+        points = 0
         if meters < 0:
             print('Количество метров не может быть отрицательным')
-            return None
         else:
-            return meters * 0.5
+            points = meters * 0.5
+        return points
 
 class TotalPoints(PointsForPlace, PointsForMeters):
     def get_total_points(self, meters, place):
         place_points = self.get_points_for_place(place)
         meter_points = self.get_points_for_meters(meters)
-        if place_points is None or meter_points is None:
-            return None
         total = place_points + meter_points
         return total
 
